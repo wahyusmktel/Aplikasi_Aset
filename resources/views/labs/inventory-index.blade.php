f3a923b8d94933ba9fcf1f1c1b986f07844f4b31
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2026-08-22 13:15:38 +0700

    refactor: replace Blade layout with component for inventory index view

--- resources/views/labs/inventory-index.blade.php
+++ resources/views/labs/inventory-index.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+<x-app-layout>
 <div class="container mx-auto p-6">
     <h1 class="text-2xl font-bold mb-4">Inventaris Lab</h1>
     <a href="{{ route('labs.create') }}" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
@@ -39,4 +37,4 @@
         </tbody>
     </table>
 </div>
-@endsection
+</x-app-layout>
